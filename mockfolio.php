4a68dd592d116c99325f154d5f0d48a350a4cae7
Alex Goodwin <alexander.goodwin@colorado.edu>
2014-12-09 02:34:46 -0700

    Added Mockfolio

---
+++ mockfolio.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title>html5 Structure</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- css -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/unsemantic-grid-responsive.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+    <link rel='stylesheet' href='css/mockfolio.css'>
+
+    <!-- Adobe Typekit -->
+    <script type="text/javascript" src="http://use.typekit.net/ote6eux.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+	<!-- Google Fonts -->
+<!-- 	<link href='http://fonts.googleapis.com/css?family=Raleway:100,800,900,400,200' rel='stylesheet' type='text/css'> -->
+
+	<!-- font awesome -->
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body class="mockFolio">
+     <section class="grid-parent topSection">
+		 <div class="header grid-100 marginAuto">
+			 <div class="grid-50 logo">
+			 	<div>
+			 		<img src="img/sunLogo.png" alt="Sun Logo">
+			 	</div>
+			 	<div>
+			       	<h1>Me Myself & I</h1>
+				 	<h2>web & graphic design portfolio</h2>
+			 	</div>
+			 </div>
+
+			 <a class="hamburger pull-right" href="#"><i class="fa fa-navicon"></i></a>
+			 <nav class="grid-50 nav">
+			 	<ul>
+			      	<li><a href="#">work</a></li>
+			 		<li><a href="#">services</a></li>
+			 		<li><a href="#">contact me</a></li>
+			 	</ul>
+			 </nav>
+		 </div>
+
+		 <div class="clearfix"></div>
+
+		 <div class="headerText grid-100 marginAuto">
+		 	<article class="grid-50">
+		 		<h2>About Me</h2>
+		 		<p>Your welcome message or “about me” text will go here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis posuere enim. Etiam ipsum odio, interdum quis lobortis eu, consequat non orci. Proin dapibus pretium augue non convallis. Aliquam sit amet nisi nec arcu bibendum imperdiet.</p>
+
+<p>Duis eleifend accumsan ligula, suscipit viverra mi tempus sed. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam diam felis, convallis a pharetra at, tristique ac ipsum. Maecenas viverra mollis est quis venenatis. Duis tincidunt lorem id augue congue adipiscing.</p>
+
+<p>Nunc et urna et magna aliquam hendrerit eu sit amet arcu. Aenean tempus sapien sagittis massa gravida id cursus lacus semper. Aliquam erat volutpat. In scelerisque, purus eu mollis laoreet, nisl elit commodo mi, non dapibus orci ipsum id dui.</p>
+		 	</article>
+
+		 	<article class="grid-50">
+		 		<h2>Looking for a Designer?</h2>
+		 		<p>I am available for freelance projects. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis posuere enim. Etiam ipsum odio, interdum quis lobortis eu, consequat non orci. Proin dapibus pretium augue non convallis. Aliquam sit amet nisi nec arcu bibendum imperdiet. Duis eleifend accumsan ligula, suscipit viverra mi tempus sed.</p>
+
+		 		<img src="img/hireme.png" id="hireMe" alt="hire me">
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis posuere enim. Etiam ipsum odio, interdum quis lobortis eu, consequat non orci.</p>
+
+				<p>email: <a href="#">mymail@mydomain.com</a></p>
+
+		 	</article>
+		 </div>
+     </section>
+
+     <section class="grid-100 middleSection marginAuto">
+     	<h2>My Work</h2>
+     	<div class="grid-33 portfolioItem">
+	 		<figure>
+	     		<img src="img/portfolioItem1.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+	 		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+     		<figure>
+     			<img src="img/portfolioItem2.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+     		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+     		<figure>
+	     		<img src="img/portfolioItem3.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+     		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+	 		<figure>
+	     		<img src="img/portfolioItem4.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+	 		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+	 		<figure>
+	     		<img src="img/portfolioItem5.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+	 		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+	 		<figure>
+	     		<img src="img/portfolioItem6.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+	 		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+	 		<figure>
+	     		<img src="img/portfolioItem7.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+	 		</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+     		<figure>
+	     		<img src="img/portfolioItem8.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+	     	</figure>
+     	</div>
+     	<div class="grid-33 portfolioItem">
+     		<figure>
+	     		<img src="img/portfolioItem9.jpg" alt="portfolio image">
+	     		<h3>Project Title</h3>
+	     		<h4>Project description and date</h4>
+     		</figure>
+     	</div>
+     </section> <!-- end middleSection -->
+
+     <section class="grid-parent bottomSection">
+     	<div class="grid-100 marginAuto">
+	     	<div class="grid-33">
+	     		<h2><i class="fa fa-pencil fa-2x"></i> Services</h2>
+
+	     		<h3>Web Design</h3>
+	     		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis posuere enim. Etiam ipsum odio, interdum quis lobortis eu, consequat non orci. Proin dapibus pretium augue non
+	convallis.</p>
+
+				<h3>Web Development</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis posuere enim. Etiam ipsum odio, interdum quis lobortis eu, consequat non orci. Proin dapibus pretium augue non
+	convallis. </p>
+
+				<h3>Graphic Design</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis posuere enim. Etiam ipsum odio, interdum quis lobortis eu, consequat non orci. Proin dapibus pretium augue non
+	convallis.</p>
+	     	</div>
+
+	     	<div class="grid-33">
+	     		<h2><i class="fa fa-twitter fa-2x"></i> Tweets</h2>
+
+	     		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis arcu, vehicula ut dignissim a, lobortis non ante.  about 3 hours ago</p>
+
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis arcu, vehicula ut dignissim a, lobortis non ante.  about 3 hours ago</p>
+
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis arcu, vehicula ut dignissim a, lobortis non ante.  about 3 hours ago</p>
+
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis arcu, vehicula ut dignissim a, lobortis non ante.  about 3 hours ago</p>
+
+		 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis arcu, vehicula ut dignissim a, lobortis non ante.  about 3 hours ago</p>
+	     	</div>
+
+	     	<div class="grid-33">
+	     		<h2><i class="fa fa-envelope fa-2x"></i> Contact</h2>
+
+	     		<p>If you’re interested in getting a quote for a
+	project or if you just have some questions, please email me with the details and I will get in touch with you as soon as possible. you can also find me on several social networking sites, my
+	profiles are listed below.</p>
+
+				<p>Email: <a href="#">mymail@mydomain.com</a></p>
+
+				<ul class="contact">
+					<li><a href="#"><i class="fa fa-twitter-square fa-2x"></i> twitter</a></li>
+					<li><a href="#"><i class="fa fa-instagram fa-2x"></i> instagram</a></li>
+					<li><a href="#"><i class="fa fa-dribbble fa-2x"></i> dribbble</a></li>
+				</ul>
+	     	</div>
+     	</div> <!-- /.bottomSection -->
+     </section>
+
+     <footer class="grid-parent">
+	     <div class="grid-100 marginAuto">
+	     	<p>Copyleft MeMyself &amp; I, 2014</p>
+	     </div>
+     </footer>
+
+	 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+	<script src="js/main.js"></script>
+
+	<!-- custom -->
+	<script>
+	$(document).ready(function(){
+
+		$("nav").css({height: "auto"});
+
+	   // hamburger nav
+	   $("a.hamburger").click(function(event) {
+			event.preventDefault();
+			$("nav ul").slideToggle(500);
+ 			$('#undefined-sticky-wrapper').css({height: 'auto !important'});
+		});
+
+		$(window).resize(function(){
+			if($(window).width() > 768){
+				$('nav ul').show();
+			}
+			else{
+				$('nav ul').hide();
+			}
+		});
+	});
+	</script>
+</body>
